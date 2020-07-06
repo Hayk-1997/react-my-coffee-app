ec701826474707edeb73a32e1f5faa42ed3216ff
Hayk <abrahamyanhayk01@gmail.com>
2020-07-07 01:40:47 +0400

    Fixed AdminLogin Validation issues

--- src/Helpers/validateForm.js
+++ src/Helpers/validateForm.js
@@ -1,20 +1,36 @@
 import validator from 'validator';
 import { notify } from '../Config/Notify';
 
-export const validateForm = (form, verifyFields, setVerifyFields) => {
+export const validateLoginForm = (form, verifyFields, setVerifyFields) => {
+  emailValidator(form, verifyFields, setVerifyFields);
+  passwordValidator(form, verifyFields, setVerifyFields);
+};
+
+
+const emailValidator = (form, verifyFields, setVerifyFields) => {
   if (!validator.isEmail(form.email)) {
-    makeValidationMessage(setVerifyFields, verifyFields, 'email', 'Incorrect Email!', 1500);
+    validationMessage( 'Incorrect Email!', 1500);
+    changeVerifyField(setVerifyFields, verifyFields, 'email', false);
+  } else {
+    changeVerifyField(setVerifyFields, verifyFields, 'email', true);
   }
+};
+
+const passwordValidator = (form, verifyFields, setVerifyFields) => {
   if (!validator.isLength(form.password, { min: 6 })) {
-    makeValidationMessage(setVerifyFields, verifyFields, 'password', 'Password Min length must be (8)', 2500);
+    validationMessage( 'Password Min length must be (6)', 2500);
+    changeVerifyField(setVerifyFields, verifyFields, 'password', false);
+  } else {
+    changeVerifyField(setVerifyFields, verifyFields, 'password', true);
   }
 };
 
 /// Validation Message Function
-const makeValidationMessage = (setVerifyFields, verifyFields, field,  message, timeout) => {
-  notify(message, timeout, 'ERROR');
-  setVerifyFields({
-    ...verifyFields,
-    [field]: false,
-  });
+const validationMessage = (message, timeout) => notify(message, timeout, 'ERROR');
+
+const changeVerifyField = (setVerifyFields, verifyFields, field, type) => {
+  setVerifyFields((prevState) => ({
+    ...prevState,
+    [field]: type,
+  }));
 };
